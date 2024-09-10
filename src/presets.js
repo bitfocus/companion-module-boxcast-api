317c498953e5dcd826406b7ee2eb5abb3e0c0eea
Joseph Adams <josephdadams@gmail.com>
2024-09-10 08:16:11 -0400

    add presets

--- src/presets.js
+++ src/presets.js
@@ -7,6 +7,10 @@ module.exports = {
 
 		const colorWhite = combineRgb(255, 255, 255) // White
 		const colorBlack = combineRgb(0, 0, 0) // Black
+		const colorRed = combineRgb(255, 0, 0) // Red
+		const colorGreen = combineRgb(0, 255, 0) // Green
+		const colorBlue = combineRgb(0, 0, 255) // Blue
+		const colorYellow = combineRgb(255, 255, 0) // Yellow
 
 		presets = [
 			{
@@ -41,6 +45,59 @@ module.exports = {
 				],
 			},
 		]
+
+		presets.currentBroadcast = {
+			category: 'Control',
+			name: 'Start Current Broadcast',
+			style: {
+				text: 'Start Current Broadcast',
+				size: '18',
+				color: colorWhite,
+				bgcolor: colorBlack,
+			},
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'startCurrentBroadcast',
+						},
+					],
+					up: [],
+				},
+			],
+			feedbacks: [
+				{
+					feedbackId: 'currentBroadcastTimeframe',
+					options: {
+						timeframe: 'future',
+					},
+					style: {
+						bgcolor: colorBlue,
+						color: colorWhite,
+					},
+				},
+				{
+					feedbackId: 'currentBroadcastTimeframe',
+					options: {
+						timeframe: 'preroll',
+					},
+					style: {
+						bgcolor: colorYellow,
+						color: colorWhite,
+					},
+				},
+				{
+					feedbackId: 'currentBroadcastTimeframe',
+					options: {
+						timeframe: 'current',
+					},
+					style: {
+						bgcolor: colorRed,
+						color: colorWhite,
+					},
+				},
+			],
+		}
 
 		self.setPresetDefinitions(presets)
 	},
